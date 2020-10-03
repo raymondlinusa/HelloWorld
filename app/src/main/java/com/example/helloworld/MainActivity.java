283501e889d80716105452eff7866a04a5db7816
raymondlinusa <raymondputra2510@gmail.com>
2020-10-03 10:38:45 +0700

    - membuat Login page - membuat home page setelah login - menambah fungsi pengecekan username dan password untuk login

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView username, password;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //simpan value dari kolom txtUsername dan txtPassword
+        username = findViewById(R.id.txtUsername);
+        password = findViewById(R.id.txtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(getApplicationContext(), username.getText()+" dan "+password.getText(), Toast.LENGTH_LONG).show();
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    setContentView(R.layout.home_screen);
+                }
+                else{
+                    failLogin();
+                }
+
+            }
+        });
+
+    }
+    private void failLogin(){
+        Toast.makeText(getApplicationContext(), "username atau password salah", Toast.LENGTH_LONG).show();
     }
 }
